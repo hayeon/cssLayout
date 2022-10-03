4375b747f6fb069bea54b2d6b2ae9fcd7021035f
hayeon <hayun4475@gmail.com>
2022-10-03 23:57:58 +0900

    css practice

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="box">1</div>
+        <div className="box">2</div>
+        <div className="box">3</div>
+        <div className="box">4</div>
+        <div className="box">5</div>
+        <div className="box">6</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
