3c5f7638d69f76e665e3c468c0467c58fd0255ec
hayeon <hayun4475@gmail.com>
2022-10-14 18:28:14 +0900

    justify-content

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 function App() {
   return (
       <div className="grid">
-        <div className="header"></div>
-        <div className="content"></div>
-        <div className="nav"></div>
-        <div className="footer"></div>
+        <div className="header">헤더</div>
+        <div className="content">콘텐츠</div>
+        <div className="nav">네비</div>
+        <div className="footer">발</div>
       </div>
   );
 }
