bb1520a4b24c8b83157b81db82782afb7a66ecbf
hayeon <hayun4475@gmail.com>
2022-10-15 23:57:26 +0900

    justify-content in grid

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ function App() {
         <div className="content">콘텐츠</div>
         <div className="nav">네비</div>
         <div className="footer">발</div>
+        <div className="header">헤더</div>
+        <div className="content">콘텐츠</div>
+        <div className="nav">네비</div>
+        <div className="footer">발</div>
+        <div className="header">헤더</div>
+        <div className="content">콘텐츠</div>
+        <div className="nav">네비</div>
+        <div className="footer">발</div>
+        <div className="header">헤더</div>
+        <div className="content">콘텐츠</div>
+        <div className="nav">네비</div>
+        <div className="footer">발</div>
+        
       </div>
   );
 }
